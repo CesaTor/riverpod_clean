856f3e3dba923aa035a1512765b10377ae3aa674
Cesare Torchia <cesare@torchia.eu>
2024-08-14 13:12:21 +0200

    add more static analysis

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_clean/core/core.dart';
-import 'package:riverpod_clean/core/router/app_router.dart';
-import 'package:riverpod_clean/core/storage/storage_provider.dart';
-import 'package:riverpod_clean/core/ui/text.dart';
-import 'package:riverpod_clean/core/ui/theme.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 

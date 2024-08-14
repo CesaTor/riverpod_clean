58bf0472b1183dbe55759060c1744998c70ecccd
Cesare Torchia <cesare@torchia.eu>
2024-08-14 10:34:12 +0200

    storage providers

---
+++ lib/core/storage/storage_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// Check if initialized in ProviderScope
+final sharedPreferencesProvider =
+    Provider<SharedPreferences>((ref) => throw UnimplementedError());
+
+final secureStorageProvider = Provider<FlutterSecureStorage>(
+  (ref) => const FlutterSecureStorage(
+    aOptions: AndroidOptions(encryptedSharedPreferences: true),
+  ),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_clean/core/router/app_router.dart';
+import 'package:riverpod_clean/core/storage/storage_provider.dart';
 import 'package:riverpod_clean/core/ui/text.dart';
 import 'package:riverpod_clean/core/ui/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
     ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(
+          await SharedPreferences.getInstance(),
+        ),
+      ],
       child: Consumer(
         builder: (context, ref, child) => MaterialApp.router(
           routerConfig: AppRouter(ref).config(),
